5c0092e38da89f5bf4d2ebb73b19ca71bba8cf5e
Enrique SV <31998587+Enrique-SV@users.noreply.github.com>
2018-03-10 12:49:17 -0600

    Update [With fewer lines of code]

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 'use strict';
-
 const webpack = require('webpack');
 const WebpackShellPlugin = require('webpack-shell-plugin');
-
 module.exports = {
-
     context: `${__dirname}/src/`,
-
     entry: {
         phaser: './phaser.js'
     },
-
     output: {
         path: `${__dirname}/build/`,
         filename: '[name].js',
@@ -21,7 +16,6 @@ module.exports = {
         devtoolFallbackModuleFilenameTemplate: "webpack:///[resource-path]?[hash]", // string
         umdNamedDefine: true,
     },
-
     module: {
         rules: [
           {
@@ -30,20 +24,14 @@ module.exports = {
           }
         ]
     },
-
     plugins: [
-
         new webpack.DefinePlugin({
             'CANVAS_RENDERER': JSON.stringify(true),
             'WEBGL_RENDERER': JSON.stringify(true)
         }),
-
         new WebpackShellPlugin({
             onBuildExit: 'node copy-to-examples.js'
         })
-
     ],
-
     devtool: 'source-map'
-
 };
